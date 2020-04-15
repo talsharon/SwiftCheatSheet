167b14e4c6b232ec67ed69b83be67e2c30b23075
Tal Sharon <talsharonts@gmail.com>
2020-04-15 19:41:02 +0300

    Initial Commit

---
+++ SwiftCheatSheet/SwiftCheatSheet.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCheatSheet.h
+//  SwiftCheatSheet
+//
+//  Created by Tal Sharon on 15/04/2020.
+//  Copyright © 2020 Tal Sharon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftCheatSheet.
+FOUNDATION_EXPORT double SwiftCheatSheetVersionNumber;
+
+//! Project version string for SwiftCheatSheet.
+FOUNDATION_EXPORT const unsigned char SwiftCheatSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCheatSheet/PublicHeader.h>
+
+
